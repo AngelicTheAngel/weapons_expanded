a4c64772c0a0e8be797f09b24e307fa64d910d17
Angelic <0andydean19@gmail.com>
2025-10-02 22:37:42 -0400

    copper

--- src/main/java/net/angelic/weaponsexpanded/item/ModItems.java
+++ src/main/java/net/angelic/weaponsexpanded/item/ModItems.java
@@ -26,6 +26,9 @@ public class ModItems {
     public static final Item STONE_BROADSWORD = registerItem("stone_broadsword",
             setting -> new Item(setting.sword(ToolMaterial.STONE, 2, -2.1F)));
 
+    public static final Item COPPER_BROADSWORD = registerItem("copper_broadsword",
+            setting -> new Item(setting.sword(ToolMaterial.COPPER, 2, -2.1F)));
+
     public static final Item IRON_BROADSWORD = registerItem("iron_broadsword",
             settings -> new Item(settings.sword(ToolMaterial.IRON, 2.0F, -2.2F)));
 
@@ -43,6 +46,9 @@ public class ModItems {
 
     public static final Item STONE_SICKLE = registerItem("stone_sickle",
             settings -> new Item(settings.sword(ToolMaterial.STONE, 2.5F, -2.3F)));
+
+    public static final Item COPPER_SICKLE = registerItem("copper_sickle",
+            settings -> new Item(settings.sword(ToolMaterial.COPPER, 2.5F, -2.3F)));
 
     public static final Item IRON_SICKLE = registerItem("iron_sickle",
             settings -> new Item(settings.sword(ToolMaterial.IRON, 2.5F, -2.3F)));
@@ -62,6 +68,9 @@ public class ModItems {
     public static final Item STONE_SCYTHE = registerItem("stone_scythe",
             settings -> new Item(settings.sword(ToolMaterial.STONE, 4.0F, -2.5F)));
 
+    public static final Item COPPER_SCYTHE = registerItem("copper_scythe",
+            settings -> new Item(settings.sword(ToolMaterial.COPPER, 4.0F, -2.5F)));
+
     public static final Item IRON_SCYTHE = registerItem("iron_scythe",
             settings -> new Item(settings.sword(ToolMaterial.IRON, 4.0F, -2.5F)));
 
@@ -79,6 +88,9 @@ public class ModItems {
 
     public static final Item STONE_LONGSWORD = registerItem("stone_longsword",
             settings -> new Item(settings.sword(ToolMaterial.STONE, 6.0F, -2.9F)));
+
+    public static final Item COPPER_LONGSWORD = registerItem("copper_longsword",
+            settings -> new Item(settings.sword(ToolMaterial.COPPER, 6.0F, -2.9F)));
 
     public static final Item IRON_LONGSWORD = registerItem("iron_longsword",
             settings -> new Item(settings.sword(ToolMaterial.IRON, 6.0F, -2.9F)));
@@ -98,6 +110,9 @@ public class ModItems {
     public static final Item STONE_KATANA = registerItem("stone_katana",
             settings -> new TwoHandedSwordItem(ToolMaterial.STONE, 3.0F, -2.2F, settings));
 
+    public static final Item COPPER_KATANA = registerItem("copper_katana",
+            settings -> new TwoHandedSwordItem(ToolMaterial.COPPER, 3.0F, -2.2F, settings));
+
     public static final Item IRON_KATANA = registerItem("iron_katana",
             settings -> new TwoHandedSwordItem(ToolMaterial.IRON, 3.0F, -2.2F, settings));
 
@@ -115,6 +130,9 @@ public class ModItems {
 
     public static final Item STONE_HATCHET = registerItem("stone_hatchet",
             settings -> new Item(settings.axe(ToolMaterial.STONE, 6.0F, -3.0F)));
+
+    public static final Item COPPER_HATCHET = registerItem("copper_hatchet",
+            settings -> new Item(settings.axe(ToolMaterial.COPPER, 6.0F, -3.0F)));
 
     public static final Item IRON_HATCHET = registerItem("iron_hatchet",
             settings -> new Item(settings.axe(ToolMaterial.IRON, 5.0F, -2.9F)));
@@ -134,6 +152,9 @@ public class ModItems {
     public static final Item STONE_HAMMER = registerItem("stone_hammer",
             settings -> new BluntWeaponItem(ModToolMaterials.FAUX_STONE, 8.0F, -3.3F, settings));
 
+    public static final Item COPPER_HAMMER = registerItem("copper_hammer",
+            settings -> new BluntWeaponItem(ModToolMaterials.FAUX_COPPER, 8.0F, -3.3F, settings));
+
     public static final Item IRON_HAMMER = registerItem("iron_hammer",
             settings -> new BluntWeaponItem(ModToolMaterials.FAUX_IRON, 7.0F, -3.2F, settings));
 
@@ -152,6 +173,9 @@ public class ModItems {
     public static final Item STONE_BATTLEAXE = registerItem("stone_battleaxe",
             settings -> new Item(settings.axe(ToolMaterial.STONE, 9.0F, -3.4F)));
 
+    public static final Item COPPER_BATTLEAXE = registerItem("copper_battleaxe",
+            settings -> new Item(settings.axe(ToolMaterial.COPPER, 9.0F, -3.4F)));
+
     public static final Item IRON_BATTLEAXE = registerItem("iron_battleaxe",
             settings -> new Item(settings.axe(ToolMaterial.IRON, 8.0F, -3.3F)));
 
@@ -169,6 +193,9 @@ public class ModItems {
 
     public static final Item STONE_GREATSWORD = registerItem("stone_greatsword",
             settings -> new TwoHandedHeavySwordItem(ToolMaterial.STONE, 8.0F, -3.3F, settings));
+
+    public static final Item COPPER_GREATSWORD = registerItem("copper_greatsword",
+            settings -> new TwoHandedHeavySwordItem(ToolMaterial.COPPER, 8.0F, -3.3F, settings));
 
     public static final Item IRON_GREATSWORD = registerItem("iron_greatsword",
             settings -> new TwoHandedHeavySwordItem(ToolMaterial.IRON, 8.0F, -3.2F, settings));
@@ -197,54 +224,63 @@ public class ModItems {
             entries.add(WOODEN_BROADSWORD);
             entries.add(GOLDEN_BROADSWORD);
             entries.add(STONE_BROADSWORD);
+            entries.add(COPPER_BROADSWORD);
             entries.add(IRON_BROADSWORD);
             entries.add(DIAMOND_BROADSWORD);
             entries.add(NETHERITE_BROADSWORD);
             entries.add(WOODEN_SICKLE);
             entries.add(GOLDEN_SICKLE);
             entries.add(STONE_SICKLE);
+            entries.add(COPPER_SICKLE);
             entries.add(IRON_SICKLE);
             entries.add(DIAMOND_SICKLE);
             entries.add(NETHERITE_SICKLE);
             entries.add(WOODEN_SCYTHE);
             entries.add(GOLDEN_SCYTHE);
             entries.add(STONE_SCYTHE);
+            entries.add(COPPER_SCYTHE);
             entries.add(IRON_SCYTHE);
             entries.add(DIAMOND_SCYTHE);
             entries.add(NETHERITE_SCYTHE);
             entries.add(WOODEN_LONGSWORD);
             entries.add(GOLDEN_LONGSWORD);
             entries.add(STONE_LONGSWORD);
+            entries.add(COPPER_LONGSWORD);
             entries.add(IRON_LONGSWORD);
             entries.add(DIAMOND_LONGSWORD);
             entries.add(NETHERITE_LONGSWORD);
             entries.add(WOODEN_KATANA);
             entries.add(GOLDEN_KATANA);
             entries.add(STONE_KATANA);
+            entries.add(COPPER_KATANA);
             entries.add(IRON_KATANA);
             entries.add(DIAMOND_KATANA);
             entries.add(NETHERITE_KATANA);
             entries.add(WOODEN_HATCHET);
             entries.add(GOLDEN_HATCHET);
             entries.add(STONE_HATCHET);
+            entries.add(COPPER_HATCHET);
             entries.add(IRON_HATCHET);
             entries.add(DIAMOND_HATCHET);
             entries.add(NETHERITE_HATCHET);
             entries.add(WOODEN_HAMMER);
             entries.add(GOLDEN_HAMMER);
             entries.add(STONE_HAMMER);
+            entries.add(COPPER_HAMMER);
             entries.add(IRON_HAMMER);
             entries.add(DIAMOND_HAMMER);
             entries.add(NETHERITE_HAMMER);
             entries.add(WOODEN_BATTLEAXE);
             entries.add(GOLDEN_BATTLEAXE);
             entries.add(STONE_BATTLEAXE);
+            entries.add(COPPER_BATTLEAXE);
             entries.add(IRON_BATTLEAXE);
             entries.add(DIAMOND_BATTLEAXE);
             entries.add(NETHERITE_BATTLEAXE);
             entries.add(WOODEN_GREATSWORD);
             entries.add(GOLDEN_GREATSWORD);
             entries.add(STONE_GREATSWORD);
+            entries.add(COPPER_GREATSWORD);
             entries.add(IRON_GREATSWORD);
             entries.add(DIAMOND_GREATSWORD);
             entries.add(NETHERITE_GREATSWORD);
