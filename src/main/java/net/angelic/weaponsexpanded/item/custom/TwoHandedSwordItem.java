007993bf9113bacbba1d887a0b9e1b3ecf446f85
Angelic <0andydean19@gmail.com>
2025-10-10 20:02:30 -0400

    tooltips for two-handed weapons

--- src/main/java/net/angelic/weaponsexpanded/item/custom/TwoHandedSwordItem.java
+++ src/main/java/net/angelic/weaponsexpanded/item/custom/TwoHandedSwordItem.java
@@ -1,17 +1,22 @@
 package net.angelic.weaponsexpanded.item.custom;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.component.type.TooltipDisplayComponent;
 import net.minecraft.item.*;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Hand;
-import net.minecraft.*;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+
+import java.util.function.Consumer;
 
 public class TwoHandedSwordItem extends Item {
 
     public TwoHandedSwordItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
         super(settings.sword(material, attackDamage, attackSpeed));
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, TooltipDisplayComponent displayComponent, Consumer<Text> textConsumer, TooltipType type) {
+        textConsumer.accept(Text.translatable("tooltip.twohandedsword"));
+
+        super.appendTooltip(stack, context, displayComponent, textConsumer, type);
     }
 }
