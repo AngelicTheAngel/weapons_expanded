81a9ed8c92b03c5e100efa5034a3e5cb69b03f8a
AngelicTheAngel <0andydean19@gmail.com>
2025-11-16 01:59:01 -0500

    update to 1.6.4

--- src/main/java/net/angelic/weaponsexpanded/datagen/ModModelProvider.java
+++ src/main/java/net/angelic/weaponsexpanded/datagen/ModModelProvider.java
@@ -1,13 +1,14 @@
 package net.angelic.weaponsexpanded.datagen;
 
 import net.angelic.weaponsexpanded.item.ModItems;
-import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.minecraft.client.data.BlockStateModelGenerator;
-import net.minecraft.client.data.ItemModelGenerator;
-import net.minecraft.client.data.Models;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
 
 public class ModModelProvider extends FabricModelProvider {
+
     public ModModelProvider(FabricDataOutput output) {
         super(output);
     }
