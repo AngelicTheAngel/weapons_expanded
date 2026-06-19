8185f992c42ef34aa9174840c91e4e4d80fd9674
andrew <0andydean19@gmail.com>
2026-06-19 13:40:34 -0400

    Fixed issue #17

---
+++ src/main/java/net/angelic/weaponsexpanded/mixin/ChainCrossbowFirstPersonMixin.java
@@ -0,0 +1,34 @@
+package net.angelic.weaponsexpanded.mixin;
+
+import net.angelic.weaponsexpanded.item.ModItems;
+import net.minecraft.client.renderer.ItemInHandRenderer;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ItemInHandRenderer.class)
+public abstract class ChainCrossbowFirstPersonMixin {
+
+    @Redirect(
+            method = "submitArmWithItem",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/ItemStack;is(Ljava/lang/Object;)Z",
+                    ordinal = 0
+            )
+    )
+    private boolean weaponsexpanded$treatChainCrossbowAsCrossbow(
+            ItemStack stack,
+            Object item
+    ) {
+        if (item == Items.CROSSBOW) {
+            return stack.is(Items.CROSSBOW)
+                    || stack.is(ModItems.CHAIN_CROSSBOW);
+        }
+
+        return item instanceof Item realItem && stack.is(realItem);
+    }
+}
